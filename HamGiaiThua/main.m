c4c68def93802fe5be3afe6b790ac7b674509e1e
le-van-mai <levanmai7@gmail.com>
2023-06-10 13:23:30 +0700

    BaitapGiaiThua

--- HamGiaiThua/main.m
+++ HamGiaiThua/main.m
@@ -7,10 +7,43 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Person : NSObject{
+    
+}
+@property int n;
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        long n;
+        long m;
+        NSLog(@"Nhap So N");
+        scanf("%ld", &n);
+        
+        if(n<0){
+            m = -n;
+        }
+        else{
+            m = n;
+        }
+        
+        long count = 1;
+        for (long i = 1; i <= m; i++) {
+            count *=i;
+        }
+        if((n<0 && n%2==0) || (n>=0)) {
+            NSLog(@"%ld! = %ld", n, count);
+            return 0;
+        }
+        if(n<0 && n%2!=0){
+            long count1 = 0 - count;
+            NSLog(@"%ld! = %ld", n, count1);
+            return 0;
+        }
+        //Giá Trị Cao Không In Được count//
+        
     }
     return 0;
 }
